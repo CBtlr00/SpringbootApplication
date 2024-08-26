b23b334ba589002c407bf7b5dac9b8fd14aa2d7b
CBtlr00 <cianbtlr3@gmail.com>
2024-08-26 16:49:18 +0100

    Update review endpoint

--- movies/src/main/java/dev/cianbtlr/movies/repo/ReviewRepository.java
+++ movies/src/main/java/dev/cianbtlr/movies/repo/ReviewRepository.java
@@ -1,4 +1,10 @@
 package dev.cianbtlr.movies.repo;
 
-public interface ReviewRepository {
+import dev.cianbtlr.movies.domain.Review;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
 }
