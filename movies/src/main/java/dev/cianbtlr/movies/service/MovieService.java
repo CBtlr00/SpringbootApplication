7dbf59a89914ad5403bf16a94a739234e0cd1de5
CBtlr00 <cianbtlr3@gmail.com>
2024-08-26 16:02:59 +0100

    Single Movies

--- movies/src/main/java/dev/cianbtlr/movies/service/MovieService.java
+++ movies/src/main/java/dev/cianbtlr/movies/service/MovieService.java
@@ -2,10 +2,12 @@ package dev.cianbtlr.movies.service;
 
 import dev.cianbtlr.movies.domain.Movie;
 import dev.cianbtlr.movies.repo.MovieRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -15,4 +17,6 @@ public class MovieService {
     public List<Movie> allMovies() {
         return movieRepository.findAll();
     }
+
+    public Optional<Movie> singleMovie(ObjectId id) { return movieRepository.findById(id); }
 }
